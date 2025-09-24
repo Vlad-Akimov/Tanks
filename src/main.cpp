c5f8675c5d8cc6eadedb2e7872a867d88985d7b9
Vlad-Akimov <121798983+Vlad-Akimov@users.noreply.github.com>
2025-09-24 19:09:13 +0300

    update CMakeLists

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,11 @@
 
 int main() {
     try {
-        // Создаем и запускаем игру с полем 40x20 клеток
         GameController game(40, 20);
         game.runGame();
     } catch (const std::exception& e) {
         std::cerr << "Ошибка: " << e.what() << std::endl;
-        return 1;
+        return -1;
     }
     
     return 0;
