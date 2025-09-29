1eeb89e5e73ed7e36efbe17508f28885767e8f35
Vlad-Akimov <121798983+Vlad-Akimov@users.noreply.github.com>
2025-09-30 01:44:47 +0300

    update GameObject

--- src/model/GameObject.cpp
+++ src/model/GameObject.cpp
@@ -18,19 +18,67 @@ class GameObject {
         bool destructible;
 
     public:
-        GameObject(Point pos, Direction dir, int spd, int hp, bool destruct);
+        GameObject(Point pos, Direction dir, int spd, int hp, bool destruct) 
+            : position(pos), direction(dir), speed(spd), health(hp), destructible(destruct) {}
+        
         virtual ~GameObject() = default;
         
         // Базовые методы
-        virtual void move(Direction dir);
-        void rotate(Direction newDirection);
-        virtual void takeDamage(int damage);
-        bool isDestroyed() const;
+        virtual void move(Direction dir) {
+            // Сохраняем предыдущую позицию на случай столкновения
+            Point prevPosition = position;
+            
+            // Обновляем направление
+            direction = dir;
+            
+            // Вычисляем новую позицию в зависимости от направления
+            switch (dir) {
+                case Direction::UP:
+                    position.y -= speed;
+                    break;
+                case Direction::DOWN:
+                    position.y += speed;
+                    break;
+                case Direction::LEFT:
+                    position.x -= speed;
+                    break;
+                case Direction::RIGHT:
+                    position.x += speed;
+                    break;
+            }
+        }
+        
+        void rotate(Direction newDirection) {
+            direction = newDirection;
+        }
+        
+        virtual void takeDamage(int damage) {
+            if (destructible) {
+                health -= damage;
+                if (health < 0) health = 0;
+            }
+        }
+        
+        bool isDestroyed() const {
+            return destructible && health <= 0;
+        }
+        
         virtual Point getBounds() const = 0; // Абстрактный метод
         
         // Виртуальные методы для переопределения
         virtual void update() = 0;
         virtual char getSymbol() const = 0; // Для отрисовки в консоли
+        
+        // Геттеры для доступа к protected полям
+        Point getPosition() const { return position; }
+        Direction getDirection() const { return direction; }
+        int getSpeed() const { return speed; }
+        int getHealth() const { return health; }
+        bool isDestructible() const { return destructible; }
+        
+        // Сеттеры
+        void setPosition(Point newPos) { position = newPos; }
+        void setHealth(int newHealth) { health = newHealth; }
 };
 
 #endif // GAMEOBJECT_CPP
